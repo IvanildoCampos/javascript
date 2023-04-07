df55311061596fccb860ac0b23b4a6c506daf29e
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2023-04-07 14:34:21 -0300

    novos trabalhos com javascript

---
+++ aula 15/vetores.js
@@ -0,0 +1,16 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+//conslole.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+*/
+for(let pos-0; pos < valores.length; pos++) {
+    console.log(`a posição ${pos} tem o valor ${valores[pos]}`)
+
+    for(let pos in valores)
+}
